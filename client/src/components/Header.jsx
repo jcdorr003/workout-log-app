170e2831da4abb500cabe06d6de2b48ca0f5e97c
J.C. Dorr <jcdorr3@JCs-MacBook-Pro.local>
2019-10-05 09:35:02 -0400

    Added react, axios, and react-router-dom.

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <h1>WeightList</h1>
+            </nav>
+        </div>
+    )
+}
+
+export default Header;
