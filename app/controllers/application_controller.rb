63318a8129f3eceb9d137b63b0e8c4e052d018e6
J.C. Dorr <jcdorr3@JCs-MacBook-Pro.local>
2020-01-18 13:27:17 -0500

    made changes to app_controller & routes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::API
     SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
 
+    def fallback_index_html
+      render :file => 'public/index.html'
+    end
+
     def encode(payload, exp = 24.hours.from_now)
         payload[:exp] = exp.to_i
         JWT.encode(payload, SECRET_KEY)
