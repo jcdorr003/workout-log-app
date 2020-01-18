d0821e703790723343ee6f4fecf849d1136ca8d8
J.C. Dorr <jcdorr3@JCs-MacBook-Pro.local>
2020-01-18 13:44:15 -0500

    Fixed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   post '/auth/login', to: 'authentication#login'
   get '/auth/verify', to: 'authentication#verify'
   resources :users
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end 
 
-get '*path', to: "application#fallback_index_html", constraints: ->(request) do
-    !request.xhr? && request.format.html?
-  end
+
