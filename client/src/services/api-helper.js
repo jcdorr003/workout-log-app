87ad380facd792fed3248ea2819d6d067abdd38a
J.C. Dorr <jcdorr3@JCs-MacBook-Pro.local>
2020-01-18 13:07:52 -0500

    Deployed to heroku.

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -1,70 +1,80 @@
-const axios = require('axios');
+const axios = require("axios");
 const api = axios.create({
-  baseURL: 'http://localhost:3000/'
+  baseURL: "http://localhost:3001/"
 });
 
-export const loginUser = async (loginData) => {
-  const resp = await api.post('/auth/login', loginData)
-  localStorage.setItem('authToken', resp.data.token);
-  api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`
-  return resp.data.user
-}
+export const loginUser = async loginData => {
+  const resp = await api.post("/auth/login", loginData);
+  localStorage.setItem("authToken", resp.data.token);
+  api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`;
+  return resp.data.user;
+};
 
-export const registerUser = async (registerData) => {
-  const resp = await api.post('/users', { user: registerData })
-  localStorage.setItem('authToken', resp.data.token);
-  api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`
-  return resp.data.user
-}
+export const registerUser = async registerData => {
+  const resp = await api.post("/users", { user: registerData });
+  localStorage.setItem("authToken", resp.data.token);
+  api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`;
+  return resp.data.user;
+};
 
 export const verifyUser = async () => {
-  const token = localStorage.getItem('authToken');
+  const token = localStorage.getItem("authToken");
   if (token) {
-    api.defaults.headers.common.authorization = `Bearer ${token}`
-    const resp = await api.get('/auth/verify');
-    return resp.data
+    api.defaults.headers.common.authorization = `Bearer ${token}`;
+    const resp = await api.get("/auth/verify");
+    return resp.data;
   }
-  return false
-}
+  return false;
+};
 
-export const destroyUser = async (id) => {
-  const resp = await api.delete(`/users/${id}`, { headers: { Authorization: `Bearer ${localStorage.getItem("jwt")}` } });
+export const destroyUser = async id => {
+  const resp = await api.delete(`/users/${id}`, {
+    headers: { Authorization: `Bearer ${localStorage.getItem("jwt")}` }
+  });
   return resp.data;
-}
+};
 
 export const showWorkouts = async () => {
-  const resp = await api.get(`/workouts`)
+  const resp = await api.get(`/workouts`);
   return resp.data;
-}
+};
 
 export const showExercises = async () => {
-  const resp = await api.get(`/exercises`)
+  const resp = await api.get(`/exercises`);
   return resp.data;
-}
+};
 
-export const showWorkout = async (id) => {
-  const resp = await api(`/workouts/${id}`)
+export const showWorkout = async id => {
+  const resp = await api(`/workouts/${id}`);
   return resp.data;
-}
+};
 
-export const postWorkout = async (item) => {
-  const resp = await api.post(`/workouts`, item, { headers: { Authorization: `Bearer ${localStorage.getItem("jwt")}` } })
-  return resp.data
-}
+export const postWorkout = async item => {
+  const resp = await api.post(`/workouts`, item, {
+    headers: { Authorization: `Bearer ${localStorage.getItem("jwt")}` }
+  });
+  return resp.data;
+};
 
 export const putWorkout = async (item, id) => {
-  const resp = await api.put(`/workouts/${id}`, item, { headers: { Authorization: `Bearer ${localStorage.getItem("jwt")}` } }, item)
-  return resp.data
-}
-export const destroyWorkout = async (id) => {
-  const resp = await api.delete(`/workouts/${id}`, { headers: { Authorization: `Bearer ${localStorage.getItem("jwt")}` } });
+  const resp = await api.put(
+    `/workouts/${id}`,
+    item,
+    { headers: { Authorization: `Bearer ${localStorage.getItem("jwt")}` } },
+    item
+  );
   return resp.data;
-}
+};
+export const destroyWorkout = async id => {
+  const resp = await api.delete(`/workouts/${id}`, {
+    headers: { Authorization: `Bearer ${localStorage.getItem("jwt")}` }
+  });
+  return resp.data;
+};
 
 export const putWorkoutExercise = async (workout_id, id) => {
-  const resp = await api.put(`/workouts/${workout_id}/exercises/${id}`, null, { headers: { Authorization: `Bearer ${localStorage.getItem("jwt")}` } })
-  return resp.data
-}
-
-
-
+  const resp = await api.put(`/workouts/${workout_id}/exercises/${id}`, null, {
+    headers: { Authorization: `Bearer ${localStorage.getItem("jwt")}` }
+  });
+  return resp.data;
+};
